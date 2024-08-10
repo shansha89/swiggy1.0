ae759528553aa67765bfe15b0c603c95a3706433
Karthick Arumugam <karthick.arumugam@xebia.com>
2024-08-10 13:11:39 +0530

    complete body with dynamic data

--- App.js
+++ App.js
@@ -20,9 +20,481 @@ const Header = () => {
     )
 }
 
+const resList = [
+    {
+        "info": {
+          "id": "526910",
+          "name": "Chinese Wok",
+          "cloudinaryImageId": "e0839ff574213e6f35b3899ebf1fc597",
+          "locality": "Agaram Main Road",
+          "areaName": "Tambaram",
+          "costForTwo": "₹250 for two",
+          "cuisines": [
+            "Chinese",
+            "Asian",
+            "Tibetan",
+            "Desserts"
+          ],
+          "avgRating": 4.2,
+          "parentId": "61955",
+          "avgRatingString": "4.2",
+          "totalRatingsString": "1K+",
+          "sla": {
+            "deliveryTime": 20,
+            "lastMileTravel": 1.2,
+            "serviceability": "SERVICEABLE",
+            "slaString": "15-20 mins",
+            "lastMileTravelString": "1.2 km",
+            "iconType": "ICON_TYPE_EMPTY"
+          },
+          "availability": {
+            "nextCloseTime": "2024-08-11 02:00:00",
+            "opened": true
+          },
+          "badges": {
+            
+          },
+          "isOpen": true,
+          "type": "F",
+          "badgesV2": {
+            "entityBadges": {
+              "imageBased": {
+                
+              },
+              "textBased": {
+                
+              },
+              "textExtendedBadges": {
+                
+              }
+            }
+          },
+          "aggregatedDiscountInfoV3": {
+            "header": "ITEMS",
+            "subHeader": "AT ₹199"
+          },
+          "orderabilityCommunication": {
+            "title": {
+              
+            },
+            "subTitle": {
+              
+            },
+            "message": {
+              
+            },
+            "customIcon": {
+              
+            }
+          },
+          "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+              "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+              "lottie": {
+                
+              },
+              "video": {
+                
+              }
+            }
+          },
+          "reviewsSummary": {
+            
+          },
+          "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          "restaurantOfferPresentationInfo": {
+            
+          },
+          "externalRatings": {
+            "aggregatedRating": {
+              "rating": "--"
+            }
+          },
+          "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+          "context": "seo-data-83a528b6-2d88-4c24-b574-2ae8a14dcd9b"
+        },
+        "cta": {
+          "link": "https://www.swiggy.com/restaurants/chinese-wok-agaram-main-road-tambaram-chennai-526910",
+          "text": "RESTAURANT_MENU",
+          "type": "WEBLINK"
+        },
+        "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+      },
+      {
+        "info": {
+          "id": "915686",
+          "name": "The Belgian Waffle Co.",
+          "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/29/c1a5910b-9e8e-45d7-9f4e-65dad86ae57b_915686.jpg",
+          "locality": "MEDAVAKKAM",
+          "areaName": "VELACHERY MAIN ROAD",
+          "costForTwo": "₹200 for two",
+          "cuisines": [
+            "Waffle",
+            "Desserts",
+            "Ice Cream",
+            "Beverages"
+          ],
+          "veg": true,
+          "parentId": "2233",
+          "avgRatingString": "--",
+          "sla": {
+            "deliveryTime": 31,
+            "lastMileTravel": 4.7,
+            "serviceability": "SERVICEABLE",
+            "slaString": "30-35 mins",
+            "lastMileTravelString": "4.7 km",
+            "iconType": "ICON_TYPE_EMPTY"
+          },
+          "availability": {
+            "nextCloseTime": "2024-08-10 23:00:00",
+            "opened": true
+          },
+          "badges": {
+            
+          },
+          "isOpen": true,
+          "type": "F",
+          "badgesV2": {
+            "entityBadges": {
+              "imageBased": {
+                
+              },
+              "textBased": {
+                
+              },
+              "textExtendedBadges": {
+                
+              }
+            }
+          },
+          "aggregatedDiscountInfoV3": {
+            "header": "20% OFF",
+            "subHeader": "UPTO ₹50"
+          },
+          "orderabilityCommunication": {
+            "title": {
+              
+            },
+            "subTitle": {
+              
+            },
+            "message": {
+              
+            },
+            "customIcon": {
+              
+            }
+          },
+          "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+              "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+              "lottie": {
+                
+              },
+              "video": {
+                
+              }
+            }
+          },
+          "reviewsSummary": {
+            
+          },
+          "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          "restaurantOfferPresentationInfo": {
+            
+          },
+          "externalRatings": {
+            "aggregatedRating": {
+              "rating": "--"
+            }
+          },
+          "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+          "context": "seo-data-83a528b6-2d88-4c24-b574-2ae8a14dcd9b"
+        },
+        "cta": {
+          "link": "https://www.swiggy.com/restaurants/the-belgian-waffle-co-medavakkam-velachery-main-road-chennai-915686",
+          "text": "RESTAURANT_MENU",
+          "type": "WEBLINK"
+        },
+        "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+      },
+      {
+        "info": {
+          "id": "745445",
+          "name": "Subway",
+          "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/4/d5ec0a30-2ec4-40d1-aad7-eb8d9ae67f97_745445.JPG",
+          "locality": "St.Thomas Mount Block",
+          "areaName": "Rajakilpakkam",
+          "costForTwo": "₹350 for two",
+          "cuisines": [
+            "Salads",
+            "Snacks",
+            "Desserts",
+            "Beverages"
+          ],
+          "avgRating": 4.1,
+          "parentId": "2",
+          "avgRatingString": "4.1",
+          "totalRatingsString": "1K+",
+          "sla": {
+            "deliveryTime": 32,
+            "lastMileTravel": 0.9,
+            "serviceability": "SERVICEABLE",
+            "slaString": "30-35 mins",
+            "lastMileTravelString": "0.9 km",
+            "iconType": "ICON_TYPE_EMPTY"
+          },
+          "availability": {
+            "nextCloseTime": "2024-08-11 03:00:00",
+            "opened": true
+          },
+          "badges": {
+            "imageBadges": [
+              {
+                "imageId": "Rxawards/_CATEGORY-Sandwiches.png",
+                "description": "Delivery!"
+              }
+            ]
+          },
+          "isOpen": true,
+          "type": "F",
+          "badgesV2": {
+            "entityBadges": {
+              "imageBased": {
+                "badgeObject": [
+                  {
+                    "attributes": {
+                      "description": "Delivery!",
+                      "imageId": "Rxawards/_CATEGORY-Sandwiches.png"
+                    }
+                  }
+                ]
+              },
+              "textBased": {
+                
+              },
+              "textExtendedBadges": {
+                
+              }
+            }
+          },
+          "aggregatedDiscountInfoV3": {
+            "header": "50% OFF",
+            "subHeader": "UPTO ₹100"
+          },
+          "orderabilityCommunication": {
+            "title": {
+              
+            },
+            "subTitle": {
+              
+            },
+            "message": {
+              
+            },
+            "customIcon": {
+              
+            }
+          },
+          "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+              "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+              "lottie": {
+                
+              },
+              "video": {
+                
+              }
+            }
+          },
+          "reviewsSummary": {
+            
+          },
+          "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          "restaurantOfferPresentationInfo": {
+            
+          },
+          "externalRatings": {
+            "aggregatedRating": {
+              "rating": "--"
+            }
+          },
+          "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+          "context": "seo-data-83a528b6-2d88-4c24-b574-2ae8a14dcd9b"
+        },
+        "cta": {
+          "link": "https://www.swiggy.com/restaurants/subway-st-thomas-mount-block-rajakilpakkam-chennai-745445",
+          "text": "RESTAURANT_MENU",
+          "type": "WEBLINK"
+        },
+        "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+      },
+      {
+        "info": {
+          "id": "659725",
+          "name": "Burger King",
+          "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/60497022-434e-4d21-bc0e-813fea770ae5_659725.JPG",
+          "locality": "CHENNAI AIRPORT",
+          "areaName": "Chrompet",
+          "costForTwo": "₹350 for two",
+          "cuisines": [
+            "Burgers",
+            "American"
+          ],
+          "avgRating": 4.3,
+          "parentId": "166",
+          "avgRatingString": "4.3",
+          "totalRatingsString": "1K+",
+          "sla": {
+            "deliveryTime": 52,
+            "lastMileTravel": 10.4,
+            "serviceability": "SERVICEABLE",
+            "slaString": "50-55 mins",
+            "lastMileTravelString": "10.4 km",
+            "iconType": "ICON_TYPE_EMPTY"
+          },
+          "availability": {
+            "nextCloseTime": "2024-08-11 04:00:00",
+            "opened": true
+          },
+          "badges": {
+            "imageBadges": [
+              {
+                "imageId": "Rxawards/_CATEGORY-Burger.png",
+                "description": "Delivery!"
+              }
+            ]
+          },
+          "isOpen": true,
+          "type": "F",
+          "badgesV2": {
+            "entityBadges": {
+              "imageBased": {
+                "badgeObject": [
+                  {
+                    "attributes": {
+                      "description": "Delivery!",
+                      "imageId": "Rxawards/_CATEGORY-Burger.png"
+                    }
+                  }
+                ]
+              },
+              "textBased": {
+                
+              },
+              "textExtendedBadges": {
+                
+              }
+            }
+          },
+          "aggregatedDiscountInfoV3": {
+            "header": "60% OFF",
+            "subHeader": "UPTO ₹120"
+          },
+          "orderabilityCommunication": {
+            "title": {
+              
+            },
+            "subTitle": {
+              
+            },
+            "message": {
+              
+            },
+            "customIcon": {
+              
+            }
+          },
+          "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+              "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+              "lottie": {
+                
+              },
+              "video": {
+                
+              }
+            }
+          },
+          "reviewsSummary": {
+            
+          },
+          "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          "restaurantOfferPresentationInfo": {
+            
+          },
+          "externalRatings": {
+            "aggregatedRating": {
+              "rating": "--"
+            }
+          },
+          "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+          "context": "seo-data-83a528b6-2d88-4c24-b574-2ae8a14dcd9b"
+        },
+        "cta": {
+          "link": "https://www.swiggy.com/restaurants/burger-king-airport-chrompet-chennai-659725",
+          "text": "RESTAURANT_MENU",
+          "type": "WEBLINK"
+        },
+        "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+      }
+]
+    
+
+const RestaurantCard = (props) => {
+    const{resData} = props
+    console.log(props);
+
+    //optional chanining
+    const {
+        cloudinaryImageId,
+        name,
+        avgRating,
+        cuisines
+    } = resData?.info;
+
+    const {      
+        slaString
+    } = resData?.info.sla; //Optional Chaining
+
+
+    return(
+        <div className="res-card" style={{backgroundColor:"#fofofo"}}>
+            <img className="res-logo"
+            src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} />
+            <h4>{name}</h4>
+            <h5>{avgRating+" Stars"}</h5>
+            <h5>{slaString}</h5>
+            <h5>{cuisines.join(", ")}</h5>
+        </div>
+    )
+}
+
+
+
 const Body = () => {
     return (
-        <div className="body"></div>
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard resData={resList[0]} />
+                <RestaurantCard resData={resList[1]} />
+                <RestaurantCard resData={resList[2]} />
+                <RestaurantCard resData={resList[3]} />
+            </div>
+        </div>
     )
 }
 
@@ -36,6 +508,7 @@ const Applayout = () => {
     return(
         <div className="app">
             <Header/>
+            <Body/>
         </div>
     )
 }
